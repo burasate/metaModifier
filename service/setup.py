84c7918b86e3838086aee21714afa14f41fa356e
burasate <61697687+burasate@users.noreply.github.com>
2024-07-03 08:51:37 +0700

    Update setup.py

--- service/setup.py
+++ service/setup.py
@@ -13,7 +13,7 @@ try:
 except:
     maya_app_dir = mel.eval('getenv MAYA_APP_DIR')
     scripts_dir = os.path.abspath(maya_app_dir + os.sep + 'scripts')
-    tool_dir = os.path.abspath(scripts_dir + os.sep + 'KFMataHModifier')
+    tool_dir = os.path.abspath(scripts_dir + os.sep + 'mhRigModifier')
 install_path = os.path.abspath(tool_dir + os.sep + 'Install.py')
 image_path = os.path.abspath(tool_dir + os.sep + 'KFMataHModifier.png')
 print([tool_dir, os.path.exists(tool_dir)])
@@ -32,3 +32,44 @@ if not os.path.exists(tool_dir) or not os.path.exists(install_path):
     '''.format(tool_dir).strip()
     cmds.confirmDialog(title='', message=error_msg, button=['OK'], icn='critical', ma='center')
     raise Warning('WARNING!!\ndo not found \"Install file\" in {}'.format(tool_dir))
+
+"""====================="""
+# Privacy Message
+"""====================="""
+privacy_msg = '''
+-------------------------------------------------------------
+Privacy Confirmation
+-------------------------------------------------------------
+
+To safeguard your privacy, I consent to the script or
+tool owner, DEX3D,  accessing my informations.    
+
+'''.strip()
+privacy_result = cmds.confirmDialog(title='MH RIG MODIFIER', message=privacy_msg, button=['Cancel','Confirm'],
+                                    defaultButton='Cancel', cancelButton='Cancel', dismissString='Cancel',
+                                    icn='warning', ma='center')
+if privacy_result != 'Confirm':
+    raise Warning('Installaition was canceled.')
+
+"""====================="""
+# Orig User Register to Files
+"""====================="""
+pt_file_path_ls = [os.path.abspath(tool_dir + os.sep + 'KFMataHModifier.py')]
+pt_file_path_ls = [i for i in pt_file_path_ls if os.path.exists(i)]
+print(pt_file_path_ls)
+'''
+for pt_path in pt_file_path_ls:
+    is_registered = False
+    with open(pt_path, 'r') as f:
+        l_read = f.readlines()
+        l_read_join = ''.join(l_read)
+        is_registered = not '$usr_orig$' in l_read_join
+        f.close()
+    if not is_registered:
+        l_read_join = l_read_join.replace('$usr_orig$', getpass.getuser())
+        with open(pt_path, 'w') as f:
+            f.writelines(l_read_join)
+            f.close()
+        print(pt_path)
+'''
+
