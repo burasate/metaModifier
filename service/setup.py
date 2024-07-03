f76f8f57131e8e64a96f28c860a44cfe70316389
burasate <61697687+burasate@users.noreply.github.com>
2024-07-03 08:57:10 +0700

    Update setup.py

--- service/setup.py
+++ service/setup.py
@@ -57,7 +57,6 @@ if privacy_result != 'Confirm':
 pt_file_path_ls = [os.path.abspath(tool_dir + os.sep + 'KFMataHModifier.py')]
 pt_file_path_ls = [i for i in pt_file_path_ls if os.path.exists(i)]
 print(pt_file_path_ls)
-for
 
 for pt_path in pt_file_path_ls:
     is_registered = False
